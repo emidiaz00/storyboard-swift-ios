6d917d32795ffa0b57107cebf14aec5c571df071
emildsn <emildsn00@gmail.com>
2023-03-03 21:50:28 -0300

    add button and modal

--- storyboard-swift-ios/ViewController.swift
+++ storyboard-swift-ios/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hola desde viewdidload")
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        let alert = UIAlertController(
+            title: "Titulo",
+            message: "Tu mensaje aqui",
+            preferredStyle: .actionSheet)
+        let ok = UIAlertAction(title: "Aceptar", style: .default) { (_) in print("Aceptar")
+        }
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
